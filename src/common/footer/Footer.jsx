374af92887083f53be6d9c9f042a97ab5ce93f65
Geronimo Dimarco <unzagageronimo@gmail.com>
2023-01-26 22:51:31 -0300

    Cambios de color y estilos en Header y Footer

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className='container grid2'>
           <div className='box'>
             <img src={logo} alt="" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
+            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p> */}
             <div className='icon d_flex'>
               <div className='img d_flex'>
                 <i class='fa-brands fa-google-play'></i>
@@ -26,13 +26,11 @@ const Footer = () => {
             <h2>Acerca de Nosotros</h2>
             <ul>
               <li>Careers</li>
-              <li>Our Stores</li>
-              <li>Our Cares</li>
-              <li>Terms & Conditions</li>
-              <li>Privacy Policy</li>
+              <li>Nuestros locales</li>
+              <li>Politicas de envios</li>
             </ul>
           </div>
-          <div className='box'>
+          {/* <div className='box'>
             <h2>Customer Care</h2>
             <ul>
               <li>Help Center </li>
@@ -41,13 +39,13 @@ const Footer = () => {
               <li>Corporate & Bulk Purchasing </li>
               <li>Returns & Refunds </li>
             </ul>
-          </div>
-          <div className='box'>
+          </div> */}
+          <div className='box-2'>
             <h2>Contactanos</h2>
             <ul>
-              <li>70 Washington Square South, New York, NY 10012, United States </li>
-              <li>Email: uilib.help@gmail.com</li>
-              <li>Phone: +1 1123 456 780</li>
+              <li>Avenida Aconquija 365, Yerba Buena, Tucuman, Argentina </li>
+              <li>Email: thaiclub@gmail.com</li>
+              <li>Phone: +54 93815541222</li>
             </ul>
           </div>
         </div>
