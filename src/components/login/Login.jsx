0968e8d35b93b4f46ff94c3d9fef8087e1cac364
Jorge Ramos <jorgerhamos.187@gmail.com>
2023-02-10 15:47:05 -0300

    corrigiendo el login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 //import { useNavigate } from "react-router-dom";
