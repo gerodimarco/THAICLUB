f66e76233a32be5d3ef50ca3e7e80472b960f289
Jorge Ramos <jorgerhamos.187@gmail.com>
2023-02-10 15:11:58 -0300

    agregando pagina de login

---
+++ src/pages/Log.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Login from "../components/login/Login"
+
+const Log = () => {
+  return (
+    <>
+      <Login />
+      
+    </>
+  )
+}
+
+export default Log
