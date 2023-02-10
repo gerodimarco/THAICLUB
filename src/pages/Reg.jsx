7f13ab049587a08132141cd329e0e1822b94fcf0
Jorge Ramos <jorgerhamos.187@gmail.com>
2023-02-10 15:14:15 -0300

    agregando pagina de register

---
+++ src/pages/Reg.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Register from "../components/regiter/Register"
+
+const Reg = () => {
+  return (
+    <>
+      <Register/>
+      
+    </>
+  )
+}
+
+export default Reg
